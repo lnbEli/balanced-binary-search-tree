5d4e2de300a5fbbe68b46ebf74fd77105805ae34
lnbEli <137424458+lnbEli@users.noreply.github.com>
2024-09-02 20:42:17 +0200

    Add delete and Insert function

--- BSTree.js
+++ BSTree.js
@@ -57,4 +57,122 @@ export default class BSTree {
       );
     }
   }
+
+  insert(value, node = this.root) {
+    //Checks value entered is number
+    if (typeof value !== "number") {
+      console.log("Enter a number!");
+      return;
+    }
+    //Value already exists and we don't want duplicates
+    if (value === node.data) {
+      console.log("Value already exists!");
+      return;
+    }
+    //Checks if tree is empty
+    if (this.root === null) {
+      this.root = new BSTNode(value);
+      return;
+    }
+    //Base case
+    if (value < node.data && node.leftChild === null) {
+      node.leftChild = new BSTNode(value);
+      return;
+    }
+    //Base case
+    if (value > node.data && node.rightChild === null) {
+      node.rightChild = new BSTNode(value);
+      return;
+    }
+    //Recursive call
+    if (value < node.data) {
+      //Traverse left subtree
+      return this.insert(value, node.leftChild);
+    } else {
+      //Traverse right subtree
+      return this.insert(value, node.rightChild);
+    }
+  }
+
+  deleteItem(value, node = this.root, parentNode = null) {
+    //If value not found or root doesn't exist. What if parent node = null for root.
+    if (node === null) {
+      return;
+    }
+
+    //Helper function to find successor node
+    function findSuccessorNode(currentNode, previousNode = null) {
+      if (currentNode.leftChild === null) {
+        return [currentNode, previousNode];
+      } else {
+        return findSuccessorNode(currentNode.leftChild, currentNode);
+      }
+    }
+
+    //If value equals node with both a left child and right child
+    if (value === node.data && node.leftChild && node.rightChild) {
+      //Check for last lowest value node in right tree
+      const [successorNode, successorParentNode] = findSuccessorNode(
+        node.rightChild,
+        node
+      );
+      //Set deleted node value to successor value
+      node.data = successorNode.data;
+      //Check if successor node is direct child node
+      if (successorParentNode === node) {
+        //Sets nodes right pointer to successor nodes right pointer.
+        node.rightChild = successorNode.rightChild;
+      } else {
+        //Set successor nodes parent node left pointer to null??
+        successorParentNode.leftChild = successorNode.rightChild;
+      }
+      return;
+    }
+
+    //When node is leaf node
+    if (
+      value === node.data &&
+      node.leftChild === null &&
+      node.rightChild === null
+    ) {
+      //Checks if parent node exists
+      if (parentNode === null) {
+        this.root = null;
+      }
+      //When node to be deleted is a leaf node on a left subtree
+      else if (parentNode.leftChild === node) {
+        parentNode.leftChild = null;
+      } else {
+        //When node to be deleted is a leaf node on a right subtree
+        parentNode.rightChild = null;
+      }
+      return;
+    }
+
+    //If value equals the nodes value and it only has one child
+    if (
+      (node.leftChild && !node.rightChild && node.data === value) ||
+      (!node.leftChild && node.rightChild && node.data === value)
+    ) {
+      // If parent node doesnt exists
+      if (parentNode === null) {
+        this.root = node.leftChild || node.rightChild;
+      }
+      //Checks whether nodes is parents left or right child
+      else if (parentNode.rightChild === node) {
+        //Sets parents node pointer to whichever child is not null
+        parentNode.rightChild = node.leftChild || node.rightChild;
+      } else {
+        parentNode.leftChild = node.leftChild || node.rightChild;
+      }
+      return;
+    }
+
+    //Recursive Call
+    if (value < node.data) {
+      return this.deleteItem(value, node.leftChild, node);
+    } else {
+      return this.deleteItem(value, node.rightChild, node);
+    }
+  }
 }
--- test.js
+++ test.js
@@ -1,8 +1,30 @@
 import BSTree from "./BSTree.js";
 const array = [
-  1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 11, 17, 14, 123, 18, 19, 14,
-  13, 15, 17, 19, 167, 16345, 1324, 1, 5, 7,
+  1, 7, 4, 23, 8, 4, 3, 5, 7, 9, 67, 6345, 324, 11, 17, 14, 123, 18, 19, 14, 13,
+  15, 17, 19, 167, 16345, 1324, 1, 5, 7,
 ];
+
+// const array = [1];
 const tree = new BSTree(array);
+
+tree.insert(124);
+tree.insert(22);
+
+tree.deleteItem(124);
+tree.deleteItem(1);
+
+tree.deleteItem(11);
+tree.deleteItem(18);
+tree.deleteItem(67);
+tree.deleteItem(13);
+tree.deleteItem(15);
+tree.deleteItem(5);
+tree.deleteItem(7);
+tree.deleteItem(3);
+tree.deleteItem(8);
+tree.deleteItem(4);
+tree.deleteItem(14);
+tree.deleteItem(9);
+tree.deleteItem(17);
 
 tree.prettyPrint();
